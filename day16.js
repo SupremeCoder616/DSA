b2061405e8162adc06e0ab045d56368df9bf7d66
supremeCoder616 <kmehra1082@gmail.com>
2025-04-09 22:29:36 +0530

    day 16 dsa

---
+++ day16.js
@@ -0,0 +1,16 @@
+for (let i = 1; i <= 5; i++) {
+  console.log(i, "For Loop");
+}
+
+let i = 1;
+while (i <= 5) {
+  console.log(i, "While loop");
+  i++;
+}
+
+let doI = 1;
+
+do {
+  console.log(doI, "Do While loop");
+  doI++;
+} while (doI >= 3);
