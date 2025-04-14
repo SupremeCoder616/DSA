073c2d8a5a5e18c2c238edc6f8952776df77fee8
supremeCoder616 <kmehra1082@gmail.com>
2025-04-14 21:27:40 +0530

    day 18 DSA

---
+++ day18.js
@@ -0,0 +1,25 @@
+/*
+Task
+Print all combinations of a 3x3 matrix using nested loops.
+
+Print the triangle star pattern shown above.
+*/
+
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     console.log(`Row ${i}, Column ${j}`);
+//   }
+// }
+
+for (let i = 1; i <= 4; ++i) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += "* ";
+  }
+  console.log(row);
+}
+
+// let i = 0;
+// console.log(++i);
+// console.log(i++);
+// console.log(i);
