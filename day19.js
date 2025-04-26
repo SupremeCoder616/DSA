ed637e5c5703ed7c301ac814c03e916b9fbf3c4e
supremeCoder616 <kmehra1082@gmail.com>
2025-04-26 23:28:15 +0530

    recursion function confusion is solved

--- day19.js
+++ day19.js
@@ -3,8 +3,18 @@ Task 2: Create your own factorial function like above
 */
 
 function factorial(n) {
-  if (n === 1) return 1;
+  if (n === 1) {
+    return 1;
+  }
   return n * factorial(n - 1);
 }
 
-console.log(factorial(3));
+console.log(factorial(5));
+
+function printReverse(n) {
+  if (n === 0) return;
+  console.log(n);
+  printReverse(n - 1);
+}
+
+// printReverse(5);
