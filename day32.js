b4e900e7f088844f4c11310e6d1266c4df78c5d7
supremeCoder616 <kmehra1082@gmail.com>
2025-07-24 20:50:29 +0530

    day 32

---
+++ day32.js
@@ -0,0 +1,36 @@
+function num(proNum) {
+  let numVal = proNum[0];
+  for (let i = 1; i < proNum.length; i++) {
+    numVal *= proNum[i];
+  }
+
+  return numVal;
+}
+// num([2, 3, 4]);
+// console.log(num([2, 3, 4]));
+
+function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiouAEIOU";
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// console.log(countVowels("hello kartik"));
+
+function isAnagram(str1, str2) {
+  console.log([...str1], [...str2].sort().join(""));
+  return [...str1].sort().join("") === [...str2].sort().join("");
+}
+
+// console.log(isAnagram("listen", "silent"));
+let stringVal = "kartik";
+// console.log([...stringVal].sort().join(""));
+
+// console.log(Math.sqrt(7));
+console.log(7 % 2 === 0);
---
+++ testing/index.js
@@ -0,0 +1,7 @@
+// console.log("kdjjd");
+
+// let ax = 10;
+// var a = 10;
+
+var a = null;
+console.log(a);
